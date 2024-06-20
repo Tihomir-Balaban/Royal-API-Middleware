75fd6d8ba2f14e2529e4ab9a992259cd369b3862
Tihomir-Balaban <TihomirBalaban1988@gmail.com>
2024-06-20 17:21:12 +0200

    feat: Product Service Interface defined

---
+++ Royal.Service/ProductService/IProductService.cs
@@ -0,0 +1,9 @@
+﻿namespace Royal.Service.ProductService;
+
+public interface IProductService
+{
+    Task<(IEnumerable<ProductDto>, bool)> GetProductsAsync(int descriptionLength, int limit, int skip);
+    Task<(ProductDto, bool)> GetProductByIdAsync(int id);
+    Task<(IEnumerable<ProductDto>, bool)> GetProductsByCategoryAndPriceAsync(string category, decimal minPrice, decimal maxPrice);
+    Task<(IEnumerable<ProductDto>, bool)> GetProductsByProductNameAsync(string productName);
+}
