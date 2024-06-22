7b1e82b2531fb6041620c47c2c7e79b23c5dc05b
Tihomir-Balaban <TihomirBalaban1988@gmail.com>
2024-06-22 12:03:47 +0200

    feat: custom base contoller

---
+++ Royal.API.Middleware/Controllers/Base/RoyalController.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+
+namespace Royal.API.Middleware.Controllers.Base
+{
+    public class RoyalController : Controller
+    {
+        internal RoyalController() : base() { }
+        private protected ActionResult<T> HttpStatusCodeResolve<T>(T responseObject, HttpStatusCode statusCode)
+        {
+            switch (statusCode)
+            {
+                case HttpStatusCode.OK:
+                    return Ok(responseObject);
+                case HttpStatusCode.BadRequest:
+                    return BadRequest(responseObject);
+                case HttpStatusCode.NoContent:
+                    return NoContent();
+                case HttpStatusCode.NotFound:
+                    return NotFound(responseObject);
+                case HttpStatusCode.InternalServerError:
+                default:
+                    return StatusCode((int)statusCode);
+            }
+        }
+    }
+}
