0f73f91e0c445e521fab8bb2ad672ef137638b19
Tihomir-Balaban <TihomirBalaban1988@gmail.com>
2024-06-22 20:07:47 +0200

    feat: Create HttpMethods static class for a more generic approach

---
+++ Royal.Service/Utilities/HttpMethods.cs
@@ -0,0 +1,25 @@
+﻿namespace Royal.Service.Utilities;
+
+internal sealed class HttpMethods
+{
+    private static readonly JsonSerializerOptions jsonSerializerOptions = new()
+    {
+        PropertyNameCaseInsensitive = true
+    };
+
+    public async static Task<(T, HttpStatusCode)> ResolveResponse<T, TService>(
+        HttpResponseMessage response,
+        ILogger<TService> logger,
+        string usedServiceMethod)
+    {
+        logger.LogInformation($"[Service = {nameof(TService)}] [Method = {usedServiceMethod}] Initializing deserialization of content");
+
+        var content = await response.Content.ReadAsStringAsync();
+
+        var item = JsonSerializer.Deserialize<T>(content, jsonSerializerOptions);
+
+        logger.LogInformation($"[Service = {nameof(TService)}] [Method = {usedServiceMethod}] Content deserialized, {nameof(item)} found");
+
+        return (item, response.StatusCode);
+    }
+}
