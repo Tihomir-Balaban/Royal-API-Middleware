71dfaa36568ff0a042a658bd305253ebe99ab6b5
Tihomir-Balaban <TihomirBalaban1988@gmail.com>
2024-06-22 20:10:58 +0200

    refactor: changes to UserDto

--- Royal.Models/Dtos/UserDto.cs
+++ Royal.Models/Dtos/UserDto.cs
@@ -1,12 +1,19 @@
-﻿using Royal.Models.Dtos.Base;
+﻿using Royal.Common.Enum;
+using Royal.Models.Dtos.Base;
+using System.Text.Json.Serialization;
 
-namespace Royal.Models.Dtos
+namespace Royal.Models.Dtos;
+
+public sealed class UserDto : BaseDto
 {
-    public sealed class UserDto : BaseDto
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string UserName { get; set; }
-        public string Email { get; set; }
-    }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Token { get; set; }
+
+    [JsonPropertyName("role")]
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public UserRole Role { get; set; }
 }
