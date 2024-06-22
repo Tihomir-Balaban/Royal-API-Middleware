132947a4b1542895a029018fd4ce362d356989a9
Tihomir-Balaban <TihomirBalaban1988@gmail.com>
2024-06-22 22:55:43 +0200

    feat: csproj and global using update

--- Royal.Service.Tests/GlobalUsings.cs
+++ Royal.Service.Tests/GlobalUsings.cs
@@ -1,5 +1,6 @@
 ﻿global using Bogus;
 global using Moq;
 global using PS = Royal.Service.ProductService;
+global using US = Royal.Service.UserService;
 global using Microsoft.Extensions.Logging;
 global using Royal.Models.Dtos;
